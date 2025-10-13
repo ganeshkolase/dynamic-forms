d6964b8c23689d142584c7a2adb1029bb34efebc
Ganesh Kolase <ganeshsk21@gmail.com>
2025-10-13 19:54:54 +0530

    feat: aligns form in the middle of the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 import {SchemaFormComponent} from './schema-based-form/schema-form.component';
+import {FormLayoutComponent} from './form-layout/form-layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [SchemaFormComponent],
+  imports: [SchemaFormComponent, FormLayoutComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss'
---
+++ src/app/form-layout/form-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-layout',
+  standalone: true,
+  templateUrl: './form-layout.component.html',
+  styleUrl: './form-layout.component.scss'
+})
+export class FormLayoutComponent {
+
+}
