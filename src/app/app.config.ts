52f26df60cbfbb2dbe30f732908342c2a277a5af
Ganesh Kolase <ganeshsk21@gmail.com>
2025-10-13 19:03:16 +0530

    refactor: optimise imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,6 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {providePrimeNG} from 'primeng/config';
-import Aura from '@primeng/themes/aura';
 import {MyPreset} from '../my-preset';
 
 
