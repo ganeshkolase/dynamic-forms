5bb9b021b3ad1ce67554a4969062849a4f48861b
Ganesh Kolase <ganeshsk21@gmail.com>
2025-10-15 11:39:55 +0530

    feat: create basic form

--- src/app/components/schema-based-form/schema-form.component.ts
+++ src/app/components/schema-based-form/schema-form.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {Message} from 'primeng/message';
-import {Schema} from '../../schema/schema';
-import registrationForm from '../../schema/sample-schema.json'
+import {FormSchema} from '../../schema/schema';
+import {userRegistrationSchema} from '../../schema/form-schems';
+import {InputText} from 'primeng/inputtext';
+import {DatePicker} from 'primeng/datepicker';
+import {Select} from 'primeng/select';
+import {MultiSelect} from 'primeng/multiselect';
+import {Checkbox} from 'primeng/checkbox';
+import {FormsModule} from '@angular/forms';
 
 
 @Component({
   selector: 'app-schema-based-form',
   imports: [
-    Message
+    Message,
+    InputText,
+    DatePicker,
+    Select,
+    MultiSelect,
+    Checkbox,
+    FormsModule
   ],
   templateUrl: './schema-form.component.html',
   standalone: true,
   styleUrl: './schema-form.component.scss'
 })
 export class SchemaFormComponent {
-  schemaForm: Schema = registrationForm
+  schemaForm: FormSchema = userRegistrationSchema as FormSchema;
+  isChecked: boolean = true;
 }
--- src/app/schema/schema.d.ts
+++ src/app/schema/schema.d.ts
@@ -1,4 +1,26 @@
-export type Schema = {
+export type FormSchema = {
   title: string;
-  fields: any[];
+  fields: FormField[];
+}
+
+export type FormFieldType =
+ 'text' |
+ 'date' |
+ 'dropdown'|
+ 'multiselect'|
+ 'checkbox' |
+ 'textarea' | 'email'
+
+export type FormValidation = {
+  pattern?: string;
+  message?: string;
+};
+
+type FormField = {
+  label: string;
+  name: string;
+  type: FormFieldType;
+  required?: boolean;
+  options?: string[];
+  validation?: FormValidation;
 }
